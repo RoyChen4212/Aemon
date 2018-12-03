0b7707f91a0cf05fdc640678abb512f308bf80dc
Roy Chen <roychen4212@gmail.com>
2018-12-03 16:15:25 -0600

    [#162285578] Add all label stories

--- stories/varys-mobile.js
+++ stories/varys-mobile.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Label from '../components/varys-mobile/label';
+import Label, { types } from '../components/varys-mobile/label';
 
 storiesOf('Label', module)
-  .add('with text', () => <Label text="I am a label" />)
+  .add('normal label', () => <Label>I am a label</Label>)
+  .add('strong label', () => <Label type={types.STRONG}>I am a strong label</Label>)
+  .add('secondary label', () => <Label type={types.SECONDARY}>I am a secondary label</Label>)
+  .add('input label', () => <Label type={types.INPUT}>I am an input label</Label>)
+  .add('clickable label', () => (
+    <Label type={types.CLICKABLE} onClick={() => alert('I was clicked')}>Click me</Label>
+  ))
