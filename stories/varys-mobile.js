4783c8abdc29bae39a5cbd85708ddc81a6872b1e
Roy Chen <roychen4212@gmail.com>
2018-12-03 19:22:31 -0600

    [#162285631] Create heading componentss

--- stories/varys-mobile.js
+++ stories/varys-mobile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Label, { types } from '../components/varys-mobile/label';
+import { H1, H2, H3 } from '../components/varys-mobile/heading';
 
 storiesOf('Label', module)
   .add('normal label', () => <Label>I am a label</Label>)
@@ -10,4 +11,9 @@ storiesOf('Label', module)
   .add('input label', () => <Label type={types.INPUT}>I am an input label</Label>)
   .add('clickable label', () => (
     <Label type={types.CLICKABLE} onClick={action('clicked')}>Click me</Label>
-  ))
+  ));
+
+storiesOf('Heading', module)
+  .add('Heading 1', () => <H1>Heading 1</H1>)
+  .add('Heading 2', () => <H2>Heading 2</H2>)
+  .add('Heading 3', () => <H3>Heading 3</H3>)
---
+++ test/varys-mobile/heading.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import {
+  H1, H2, H3
+} from '../../components/varys-mobile/heading';
+
+describe('Heading', () => {
+  it('should render an H1 tag when using H1', () => {
+    const wrapper = shallow(<H1 />);
+    expect(wrapper.find('h1')).to.have.lengthOf(1);
+  });
+
+  it('should contain correct class when using H1', () => {
+    const wrapper = shallow(<H1 />);
+    expect(wrapper.find('h1').hasClass('pbg-heading')).to.be.true;
+    expect(wrapper.find('h1').hasClass('pbg-h1')).to.be.true;
+  });
+
+  it('should render an H2 tag when using H2', () => {
+    const wrapper = shallow(<H2 />);
+    expect(wrapper.find('h2')).to.have.lengthOf(1);
+  });
+
+  it('should contain correct class when using H2', () => {
+    const wrapper = shallow(<H2 />);
+    expect(wrapper.find('h2').hasClass('pbg-heading')).to.be.true;
+    expect(wrapper.find('h2').hasClass('pbg-h2')).to.be.true;
+  });
+
+  it('should render an H2 tag when using H3', () => {
+    const wrapper = shallow(<H3 />);
+    expect(wrapper.find('h3')).to.have.lengthOf(1);
+  });
+
+  it('should contain correct class when using H3', () => {
+    const wrapper = shallow(<H3 />);
+    expect(wrapper.find('h3').hasClass('pbg-heading')).to.be.true;
+    expect(wrapper.find('h3').hasClass('pbg-h3')).to.be.true;
+  });
+});
