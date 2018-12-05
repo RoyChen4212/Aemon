21f86233afddb1f901d3200d50ffd123de078aab
Roy Chen <roychen4212@gmail.com>
2018-12-05 11:27:08 -0600

    [#162285631] Refactor button to add subclasses

--- components/varys-mobile/button/index.js
+++ components/varys-mobile/button/index.js
@@ -1,1 +1,10 @@
-export * from './primary-button';
+import React from 'react';
+import Button from './button';
+
+export const PrimaryButton = (props) => (
+  <Button {...props} className='pbg-button-primary'>{props.children}</Button>
+);
+
+export const SecondaryButton = (props) => (
+  <Button {...props} className='pbg-button-secondary'>{props.children}</Button>
+);
--- test/varys-mobile/button.test.js
+++ test/varys-mobile/button.test.js
@@ -6,65 +6,72 @@ import sinon from 'sinon';
 import {
   PrimaryButton, //SecondaryButton, SmallButton, LinkButton, FacebookButton,
 } from '../../components/varys-mobile/button';
+import Button from '../../components/varys-mobile/button/button';
 import Hint from '../../components/varys-mobile/hint';
 
 describe('Button', () => {
-  describe('Primary button', () => {
-    it('should render a button tag when using PrimaryButton', () => {
-      const wrapper = shallow(<PrimaryButton />);
+  describe('Base Button', () => {
+    it('should render a button tag', () => {
+      const wrapper = shallow(<Button />);
       expect(wrapper.find('button')).to.have.lengthOf(1);
+    });
+
+    it('should have correct class name', () => {
+      const wrapper = shallow(<Button />);
+      expect(wrapper.find('button').hasClass('pbg-button')).to.be.true;
+    });
+
+    it('should add disabled class when disabled prop is present', () => {
+      const wrapper = shallow(<Button disabled />);
+      expect(wrapper.find('button').hasClass('disabled')).to.be.true;
     });
 
     it('should render correct text', () => {
       const expected = 'some text';
-      const wrapper = shallow(<PrimaryButton>{expected}</PrimaryButton>)
+      const wrapper = shallow(<Button>{expected}</Button>)
       expect(wrapper.find('button').text()).to.equal(expected);
-    });
-
-    it('should have correct class name', () => {
-      const wrapper = shallow(<PrimaryButton />);
-      expect(wrapper.find('button').hasClass('pbg-button')).to.be.true;
-      expect(wrapper.find('button').hasClass('pbg-button-primary')).to.be.true;
-    });
-
-    it('should add disabled class when disabled prop is present', () => {
-      const wrapper = shallow(<PrimaryButton disabled />);
-      expect(wrapper.find('button').hasClass('disabled')).to.be.true;
     });
 
     it('should execute click handler if given', () => {
       const onClick = sinon.spy();
-      const wrapper = shallow(<PrimaryButton onClick={onClick} />);
+      const wrapper = shallow(<Button onClick={onClick} />);
       wrapper.find('button').simulate('click');
       expect(onClick.calledOnce).to.be.true;
     });
 
     it('should not execute click handler if given but disabled', () => {
       const onClick = sinon.spy();
-      const wrapper = shallow(<PrimaryButton disabled onClick={onClick} />);
+      const wrapper = shallow(<Button disabled onClick={onClick} />);
       wrapper.find('button').simulate('click');
       expect(onClick.calledOnce).to.be.false;
     });
 
     it('should have type button if click handler is given', () => {
-      const wrapper = shallow(<PrimaryButton onClick={() => {}} />);
+      const wrapper = shallow(<Button onClick={() => {}} />);
       expect(wrapper.find('button').html()).to.include('type="button"');
     });
 
     it('should have type submit if click handler is not given', () => {
-      const wrapper = shallow(<PrimaryButton />);
+      const wrapper = shallow(<Button />);
       expect(wrapper.find('button').html()).to.include('type="submit"');
     });
 
     it('should render hint if provided', () => {
       const expected = 'Hint';
-      const wrapper = shallow(<PrimaryButton hint={expected}>Button</PrimaryButton>);
+      const wrapper = shallow(<Button hint={expected}>Button</Button>);
       expect(wrapper.contains(<Hint>{expected}</Hint>)).to.be.true;
     });
 
     it('should not render hint if not provided', () => {
-      const wrapper = shallow(<PrimaryButton>Button</PrimaryButton>);
+      const wrapper = shallow(<Button>Button</Button>);
       expect(wrapper.html()).to.not.include('<span class="pbg-hint">');
+    });
+  });
+
+  describe('Primary button', () => {
+    it('should have correct class name', () => {
+      const wrapper = shallow(<PrimaryButton />);
+      expect(wrapper.find(Button).hasClass('pbg-button-primary')).to.be.true;
     });
   });
 });
