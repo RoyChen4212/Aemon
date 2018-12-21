7e28425412140d89fff194e7924df5a07fdbb8e3
Roy Chen <roychen4212@gmail.com>
2018-12-21 09:16:24 -0600

    [#162795338] Copy svg files to build

--- components/consumer/mobile/index.js
+++ components/consumer/mobile/index.js
@@ -1,17 +1,53 @@
-import formFields from './form-fields';
-import buttons from './button';
-import headings from './heading';
+import {
+  AddressField,
+  Checkbox,
+  DatePicker,
+  DatetimePicker,
+  HistoricalPicker,
+  PasswordField,
+  Picker,
+  TextField,
+  TimePicker,
+  NewAddressField,
+} from './form-fields';
+import {
+  PrimaryButton,
+  SecondaryButton,
+  SmallButton,
+  LinkButton,
+  FacebookButton,
+  SmallFacebookButton,
+} from './button';
+import {
+  H1,
+  H2,
+  H3,
+} from './heading';
 import Hint, { hintTypes } from './hint';
 import Label, { labelTypes } from './label';
 
 const mobile = {
-  ...formFields,
-  ...buttons,
-  ...headings,
-  Hint,
-  hintTypes,
-  Label,
-  labelTypes,
+  AddressField,
+  Checkbox,
+  DatePicker,
+  DatetimePicker,
+  HistoricalPicker,
+  PasswordField,
+  Picker,
+  TextField,
+  TimePicker,
+  NewAddressField,
+  PrimaryButton,
+  SecondaryButton,
+  SmallButton,
+  LinkButton,
+  FacebookButton,
+  SmallFacebookButton,
+  H1,
+  H2,
+  H3,
+  Hint, hintTypes,
+  Label, labelTypes,
 };
 
 export default mobile;
--- dist/consumer/mobile/index.js
+++ dist/consumer/mobile/index.js
@@ -5,11 +5,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _formFields = _interopRequireDefault(require("./form-fields"));
+var _formFields = require("./form-fields");
 
-var _button = _interopRequireDefault(require("./button"));
+var _button = require("./button");
 
-var _heading = _interopRequireDefault(require("./heading"));
+var _heading = require("./heading");
 
 var _hint = _interopRequireWildcard(require("./hint"));
 
@@ -17,18 +17,30 @@ var _label = _interopRequireWildcard(require("./label"));
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var mobile = _objectSpread({}, _formFields.default, _button.default, _heading.default, {
+var mobile = {
+  AddressField: _formFields.AddressField,
+  Checkbox: _formFields.Checkbox,
+  DatePicker: _formFields.DatePicker,
+  DatetimePicker: _formFields.DatetimePicker,
+  HistoricalPicker: _formFields.HistoricalPicker,
+  PasswordField: _formFields.PasswordField,
+  Picker: _formFields.Picker,
+  TextField: _formFields.TextField,
+  TimePicker: _formFields.TimePicker,
+  NewAddressField: _formFields.NewAddressField,
+  PrimaryButton: _button.PrimaryButton,
+  SecondaryButton: _button.SecondaryButton,
+  SmallButton: _button.SmallButton,
+  LinkButton: _button.LinkButton,
+  FacebookButton: _button.FacebookButton,
+  SmallFacebookButton: _button.SmallFacebookButton,
+  H1: _heading.H1,
+  H2: _heading.H2,
+  H3: _heading.H3,
   Hint: _hint.default,
   hintTypes: _hint.hintTypes,
   Label: _label.default,
   labelTypes: _label.labelTypes
-});
-
+};
 var _default = mobile;
 exports.default = _default;
