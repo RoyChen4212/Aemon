a442595b046c4a5c5ae710b40e78e868ac4829ff
Roy Chen <roychen4212@gmail.com>
2018-12-25 13:11:28 -0600

    [#162645132] Extract similar add or select field logic into its own class and reuse

--- components/consumer/mobile/phone-field/phone-field.js
+++ components/consumer/mobile/phone-field/phone-field.js
@@ -1,39 +1,23 @@
 import React from 'react';
 import get from 'lodash/get';
-import { HistoricalPicker, TextField } from '../form-fields';
-import { SmallButton } from '../button';
-import FormField from '../form-field';
+import { TextField } from '../form-fields';
+import AddOrSelectField from '../add-or-select-field';
 import makeEvent from '../../../lib/make-event';
 import './style.css';
 
-class PhoneField extends FormField {
+class PhoneField extends AddOrSelectField {
   baseClassName = 'pbg-form-field pbg-phone-field';
   state = {
     phoneTouched: false,
   }
 
-  get className() {return this.baseClassName; }
-
-  get addingNew() {
-    const selected = get(this.props, 'value.selected');
-    return selected === 'new';
-  }
-
-  get addNewButton() {
-    if (this.addingNew) return null;
-    return (
-      <SmallButton onClick={() => this.updateValue({ selected: 'new' })}>
-        {this.props.addNewButtonLabel}
-      </SmallButton>
-    );
-  }
+  get options() { return this.props.phoneOptions; }
 
   get phoneError() {
     return this.state.phoneTouched ? get(this.props, 'error.phone') : null;
   }
 
-  get newPhoneTextField() {
-    if (!this.addingNew) return null;
+  get field() {
     return (
       <TextField
         label={this.props.addPhoneLabel}
@@ -44,33 +28,12 @@ class PhoneField extends FormField {
     )
   }
 
-  updateValue = (value) => {
-    const newValue = {
-      ...this.props.value,
-      ...value,
-    };
-    this.onChange(makeEvent(newValue));
-  }
-
   touchPhone = () => {
     const newState = { phoneTouched: true };
     this.setState(newState, () => {
       this.onBlur(makeEvent(this.props.value));
       this.forceUpdate();
     });
-  }
-
-  render() {
-    return (
-      <div className={this.className}>
-        <HistoricalPicker
-          options={this.props.phoneOptions}
-          value={get(this.props, 'value.selected')}
-        />
-        { this.newPhoneTextField }
-        { this.addNewButton }
-      </div>
-    )
   }
 }
 
