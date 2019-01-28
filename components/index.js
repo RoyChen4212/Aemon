a22ec40d937fbbb560188d6ef3aed3aede50f226
Roy Chen <roychen4212@gmail.com>
2019-01-28 17:27:18 -0600

    [#163064710] Expose consumer desktop

--- components/index.js
+++ components/index.js
@@ -1,6 +1,11 @@
 import mobile from './consumer/mobile';
+import desktop from './consumer/desktop';
+
 const consumerMobile = mobile;
+const consumerDesktop = desktop;
+
 export {
+  consumerDesktop,
   consumerMobile,
 };
 
