5d7cfa799812630b05d647ddbf1a38130ba4b41c
Roy Chen <roychen4212@gmail.com>
2019-02-04 18:22:09 -0600

    [#163067075] Add pbg-consumer-mobile namespace

--- components/consumer/mobile/hint/index.js
+++ components/consumer/mobile/hint/index.js
@@ -5,7 +5,7 @@ import './style.css'
 export * from '../../shared/hint';
 export default (props) => {
   return (
-    <div>
+    <div className="pbg-consumer-mobile">
       <Hint {...props} />
     </div>
   );
