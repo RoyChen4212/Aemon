e0b04427ddddd96136db29face7e94609115ad84
Roy Chen <roychen4212@gmail.com>
2019-02-13 11:08:30 -0600

    [#163067349] Create add comment component

---
+++ stories/consumer/desktop/add-comment.story.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { WithFigma } from 'storybook-addon-figma';
+import AddComment from '../../../components/consumer/desktop/add-comment'
+import { withContainer, wrapStory } from '../../util/decorators';
+
+import '../../style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+const figmaUrl = 'https://www.figma.com/file/XpekCUXwdO46PcY2mqkmgATD/pbg-desktop?node-id=110%3A128';
+
+storiesOf('Consumer/Desktop/Form Components/AddComment', module)
+  .addDecorator(storyFn => <WithFigma url={figmaUrl}>{storyFn()}</WithFigma>)
+  .addDecorator(wrapStory)
+  .addDecorator(withContainer)
+  .add('Default', () => (
+    <AddComment
+      hint="Only logged-in group members see comments."
+      ctaLabel="Post comment"
+      textLabel="Write a comment..."
+      avatarSrc='http://i.pravatar.cc/300'
+    />
+  ))
+  .add('With Error', () => (
+    <AddComment
+      hint="Only logged-in group members see comments."
+      ctaLabel="Post comment"
+      textLabel="Write a comment..."
+      error="This is a bad error"
+      avatarSrc='http://i.pravatar.cc/300'
+    />
+  ));
