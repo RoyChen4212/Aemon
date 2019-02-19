dcef7d32299f07702955ccce2c694b05bf114c42
Roy Chen <roychen4212@gmail.com>
2019-02-19 17:05:46 -0600

    Show phone field error whenever is sent

--- components/consumer/mobile/phone-field/phone-field.js
+++ components/consumer/mobile/phone-field/phone-field.js
@@ -3,24 +3,15 @@ import get from 'lodash/get';
 import { TextField } from '../form-fields';
 import AddOrSelectField from '../add-or-select-field';
 import makeEvent from '../../../lib/make-event';
-import touchField from '../../../lib/touch-field';
 import './style.css';
 
 class PhoneField extends AddOrSelectField {
   baseClassName = 'pbg-form-field pbg-phone-field';
-  state = {
-    phoneTouched: false,
-  }
-
-  constructor(props) {
-    super(props);
-    this.touchField = touchField.bind(this);
-  }
 
   get options() { return this.adaptedProps.phoneOptions; }
 
   get phoneError() {
-    return this.state.phoneTouched ? get(this.adaptedProps, 'error.phone') : null;
+    return get(this.adaptedProps, 'error.phone');
   }
 
   get phoneValue() { return get(this.value, 'phone'); }
@@ -31,17 +22,12 @@ class PhoneField extends AddOrSelectField {
         label={this.adaptedProps.addPhoneLabel}
         onChange={(ev) => this.updateValue({ phone: ev.target.value})}
         value={this.phoneValue}
-        onBlur={this.touchPhone}
+        onBlur={this.onBlur}
         type="tel"
         pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
         error={this.phoneError}
       />
     )
-  }
-
-  touchPhone = () => {
-    const newState = { phoneTouched: true };
-    return this.touchField(newState);
   }
 }
 
