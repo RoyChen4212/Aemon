e24ee009f0ce3865a0fead6794766c2551bb9f96
Roy Chen <roychen4212@gmail.com>
2019-02-20 09:38:54 -0600

    Update tests

--- test/consumer/desktop/activity-card.test.js
+++ test/consumer/desktop/activity-card.test.js
@@ -26,15 +26,15 @@ describe('ActivityCard', () => {
     it('should render a description', () => {
       const title = "Purchase updated";
       const description = "User changed the price from $2,000 to $20,000.";
-      const wrapper = shallow(<ActivityCard type="purchase_updated" title={title} description={description} />);
-      expect(wrapper.html()).to.include(description);
+      const wrapper = shallow(<ActivityCard type="purchase_updated" title={title}>{description}</ActivityCard>);
+      expect(wrapper.contains(description)).to.be.true;
     });
 
     it('should render the time of the activity', () => {
       const title = "Purchase updated";
       const description = "User changed the price from $2,000 to $20,000.";
       const dateTime = new Date("2018-01-08T14:57:00Z");
-      const wrapper = shallow(<ActivityCard type="purchase_updated" title={title} description={description} time={dateTime} />);
+      const wrapper = shallow(<ActivityCard type="purchase_updated" title={title} time={dateTime}>{description}</ActivityCard>);
       expect(wrapper.html()).to.include(moment(dateTime).format("hh:mm A"));
     });
   });
