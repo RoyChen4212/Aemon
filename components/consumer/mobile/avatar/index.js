30e2ed13949396119a6ded1cea604ad6cc1b26f9
Roy Chen <roychen4212@gmail.com>
2019-04-10 11:31:09 -0500

    [#164693928] Fix Avatar default size

--- components/consumer/mobile/avatar/index.js
+++ components/consumer/mobile/avatar/index.js
@@ -1,3 +1,3 @@
 import React from 'react';
 import Avatar from '../../shared/avatar';
-export default (props) => <Avatar {...props} className="pbg-consumer-mobile" size={props.size || 30} />;
+export default (props) => <Avatar {...props} className="pbg-consumer-mobile" size={props.size || 32} />;
