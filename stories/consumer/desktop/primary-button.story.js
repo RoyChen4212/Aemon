40d7e0971075c5c52d78c6895d54827ab8b31dfe
Roy Chen <roychen4212@gmail.com>
2019-05-29 16:37:44 -0500

    [#166066267] Update button styles on desktop and create facebook button

--- stories/consumer/desktop/primary-button.story.js
+++ stories/consumer/desktop/primary-button.story.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { WithFigma } from 'storybook-addon-figma';
-import { PrimaryButton } from '../../../components/consumer/desktop/button';
+import { PrimaryButton, FacebookButton } from '../../../components/consumer/desktop/button';
 import { withContainer, wrapStory } from '../../util/decorators';
 
 import '../../style.css';
@@ -10,13 +10,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const figmaUrl = 'https://www.figma.com/file/XpekCUXwdO46PcY2mqkmgATD/pbg-desktop?node-id=0%3A16729';
 
-storiesOf('Consumer/Desktop/Atomic Components/Primary Button', module)
+storiesOf('Consumer/Desktop/Atomic Components/Buttons', module)
   .addDecorator(storyFn => <WithFigma url={figmaUrl}>{storyFn()}</WithFigma>)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('Enabled', () => (
-    <PrimaryButton onClick={action('clicked')}>Primary Button</PrimaryButton>
+  .add('cta-primary/normal', () => (
+    <React.Fragment>
+      <PrimaryButton onClick={action('clicked')}>Primary Button</PrimaryButton>
+      <p>Mouse over for hover, click for pressed</p>
+    </React.Fragment>
   ))
-  .add('Disabled', () => (
+  .add('cta-primary/submitting', () => (
     <PrimaryButton disabled onClick={action('clicked')}>Primary Button</PrimaryButton>
+  ))
+  .add('facebook-button/default', () => (
+    <FacebookButton onClick={action('clicked')}>Continue with Facebook</FacebookButton>
   ));
