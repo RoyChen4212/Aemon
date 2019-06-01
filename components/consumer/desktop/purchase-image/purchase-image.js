f27a490c675797628d72dc66d409bb62f58cd338
Roy Chen <roychen4212@gmail.com>
2019-05-31 21:53:36 -0500

    [#166100470] Create purchase-image component.

---
+++ components/consumer/desktop/purchase-image/purchase-image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.css';
+
+const PurchaseImage = props => (
+  <img className="pbg-consumer-desktop pbg-purchase-image" src={props.src} />
+);
+
+export default PurchaseImage;
