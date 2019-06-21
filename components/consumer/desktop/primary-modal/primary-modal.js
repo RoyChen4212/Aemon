01c02b7704143d0462859e947f2f50619394ddb6
Roy Chen <roychen4212@gmail.com>
2019-06-21 15:44:16 -0300

    [#166100335] Resize primary-modal footer based on the content

--- components/consumer/desktop/primary-modal/primary-modal.js
+++ components/consumer/desktop/primary-modal/primary-modal.js
@@ -34,12 +34,14 @@ class PrimaryModal extends React.PureComponent {
         onHideAlert={this.props.onHideAlert}
       >
         <div className="pbg-primary-modal-content">
-          <div className="pbg-primary-modal-col">
+          <div className="pbg-primary-modal-main-container">
             <div className="pbg-primary-modal-main-content">{this.props.mainContent}</div>
-            <Divider />
-            <div className="pbg-primary-modal-footer-content">{this.props.footerContent}</div>
+            <div>
+              <Divider />
+              <div className="pbg-primary-modal-footer-content">{this.props.footerContent}</div>
+            </div>
           </div>
-          <div className="pbg-primary-modal-sidebar-content pbg-primary-modal-col">{this.props.sidebarContent}</div>
+          <div className="pbg-primary-modal-sidebar-content">{this.props.sidebarContent}</div>
         </div>
       </Modal>
     );
