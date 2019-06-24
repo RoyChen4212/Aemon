0305fe577e236cd504caffe124bec10a6c408672
Roy Chen <roychen4212@gmail.com>
2019-06-24 17:54:51 -0300

    [#165423951] Replace unsupported feature String.padStart

--- components/consumer/desktop/money-field/toCurrency.js
+++ components/consumer/desktop/money-field/toCurrency.js
@@ -1,12 +1,14 @@
+import padStart from 'lodash/padStart';
+
 const getDigitsFromValue = value => value.replace(/(-(?!\d))|[^0-9|-]/g, '') || '';
 const removeLeadingZeros = number => number.replace(/^0+([0-9]+)/, '$1');
 
 const formatCurrency = (digits, separator) => {
-  const number = digits.padStart(4, '0');
+  const number = padStart(digits, 4, '0');
   const centsPosition = number.length - 2;
   const dollars = removeLeadingZeros(number.substring(0, centsPosition));
   const cents = number.substring(centsPosition);
-  return dollars.padStart(2, '0') + separator + cents;
+  return padStart(dollars, 2, '0') + separator + cents;
 };
 
 export default (value = '', separator = '.') => {
