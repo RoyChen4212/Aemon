55f1a9eaa92149da7ea1722240b5be53f4273f49
Roy Chen <roychen4212@gmail.com>
2019-06-24 20:36:19 -0500

    [#165423277] Add password-field component.
    Extend `text-field` component:
    
    * Add `password` as `input` type
    * Accepts a `forgotPasswordText` prop to render a clickable

---
+++ components/consumer/desktop/password-field/password-field.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { TextField } from '../text-field';
+import Hint, { hintTypes } from '../hint';
+
+import './style.css';
+
+class PasswordField extends TextField {
+  baseClassName = 'pbg-consumer-desktop pbg-form-field pbg-text-field pbg-password-field';
+
+  baseType = 'password';
+
+  renderForgotPassword = () => {
+    if (!this.props.forgotPasswordText) return null;
+    return (
+      <Hint type={hintTypes.CLICKABLE} onClick={this.adaptedProps.onForgotPassword}>
+        {this.props.forgotPasswordText}
+      </Hint>
+    );
+  };
+
+  renderLabel = Label => {
+    if (this.props.simple) return null;
+
+    const { label, forgotPasswordText } = this.props;
+    const header = (
+      <div className="pbg-password-field-header">
+        <Label type={this.labelType} required={this.props.required}>
+          {label}
+        </Label>
+        {this.renderForgotPassword()}
+      </div>
+    );
+    return label || forgotPasswordText ? header : null;
+  };
+}
+
+export default PasswordField;
