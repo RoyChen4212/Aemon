6c833131dbdd59cb78ec3707c764ff43c09ce291
Roy Chen <roychen4212@gmail.com>
2019-07-04 02:27:24 +0400

    [#166271925] Fixed Styling And Refactor To Not Create new Variable on Each use of component

--- components/consumer/desktop/facebook-input/facebook-input.js
+++ components/consumer/desktop/facebook-input/facebook-input.js
@@ -3,20 +3,19 @@ import React  from 'react';
 import {FacebookButton} from "../button";
 import './style.scss';
 
-const FacebookInput = (props) => {
-  const baseClassName = 'pbg-consumer-desktop pbg-facebook-input';
-  return (
-    <div className={baseClassName}>
-      <FacebookButton {...props}>
-        Continue with Facebook
-      </FacebookButton>
-      <p
-        className="pbg-desktop-secondary-text pbg-desktop-small-text"
-      >
-        You previously logged in with Facebook. Please click to authenticate again.
-      </p>
-    </div>
-  )
-};
+const baseClassName = 'pbg-consumer-desktop pbg-facebook-input';
+
+const FacebookInput = (props) => (
+  <div className={baseClassName}>
+    <FacebookButton {...props}>
+      Continue with Facebook
+    </FacebookButton>
+    <p
+      className="pbg-desktop-secondary-text pbg-desktop-small-text"
+    >
+      You previously logged in with Facebook. Please click to authenticate again.
+    </p>
+  </div>
+);
 
 export default FacebookInput;
