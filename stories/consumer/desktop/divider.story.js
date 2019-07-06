24745de01977b1b5df1a555b4e38fffc4d6661ed
Roy Chen <roychen4212@gmail.com>
2019-07-06 10:05:50 +0300

    [#166271872] add two states of divider component

--- stories/consumer/desktop/divider.story.js
+++ stories/consumer/desktop/divider.story.js
@@ -6,7 +6,8 @@ import { withContainer, wrapStory } from '../../util/decorators';
 import '../../style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-storiesOf('Consumer/Desktop/Atomic Components', module)
+storiesOf('Consumer/Desktop/Atomic Components/divider', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('divider', () => <Divider label='Label' hint='Helper text' />)
+  .add('divider/label-only', () => <Divider label='Label' />)
+  .add('divider/label-with-helper', () => <Divider label='Label' hint='Helper text' />)
