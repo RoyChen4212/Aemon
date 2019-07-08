003bc4460fe5ef4878b2e27331642faff2591e11
Roy Chen <roychen4212@gmail.com>
2019-07-08 14:44:01 +0400

    [#166333220] Add div-input element

---
+++ stories/consumer/desktop/div-input.story.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withContainer, wrapStory } from '../../util/decorators';
+import DivInput from '../../../components/consumer/desktop/div-input/div-input';
+
+import '../../style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+storiesOf('Consumer/Desktop/Atomic Components/Inputs', module)
+  .addDecorator(wrapStory)
+  .addDecorator(withContainer)
+  .add('div-input', () => <DivInput />);
