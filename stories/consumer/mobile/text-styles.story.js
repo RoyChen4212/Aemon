6cfae672ff56a06cd3021f877ec9f9dfa0392284
Roy Chen <roychen4212@gmail.com>
2019-07-08 20:47:05 +0400

    [#167084183] Fix changed the placement and names of the storybook items

--- stories/consumer/mobile/text-styles.story.js
+++ stories/consumer/mobile/text-styles.story.js
@@ -6,15 +6,15 @@ import { withContainer, wrapStory } from '../../util/decorators';
 import '../../style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-storiesOf('Consumer/Mobile/Text Styles', module)
+storiesOf('Consumer/Mobile/Atomic Components/Typography', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('Heading 1', () => <div className="pbg-mobile-heading-1">Heading 1</div>)
-  .add('Heading 2', () => <div className="pbg-mobile-heading-2">Heading 2</div>)
-  .add('P', () => <div className="pbg-mobile-paragraph">Paragraph</div>)
-  .add('Label/strong', () => <div className="pbg-mobile-label-strong">Label Strong</div>)
-  .add('Label/clickable', () => <div className="pbg-mobile-label-link">Label Link</div>)
-  .add('Label/secondary', () => <div className="pbg-mobile-label-secondary">Label Secondary</div>)
-  .add('little/normal', () => <div className="pbg-mobile-small-text">Small Text</div>)
-  .add('little/secondary', () => <div className="pbg-mobile-small-secondary">Small Secondary Text</div>)
-  .add('hint/normal', () => <div className="pbg-mobile-hint-normal">Hint Normal</div>);
+  .add('pbg-mobile-heading-1', () => <div className="pbg-mobile-heading-1">Heading 1</div>)
+  .add('pbg-mobile-heading-2', () => <div className="pbg-mobile-heading-2">Heading 2</div>)
+  .add('pbg-mobile-paragraph', () => <div className="pbg-mobile-paragraph">Paragraph</div>)
+  .add('pbg-mobile-label-strong', () => <div className="pbg-mobile-label-strong">Label Strong</div>)
+  .add('pbg-mobile-label-link', () => <div className="pbg-mobile-label-link">Label Link</div>)
+  .add('pbg-mobile-label-secondary', () => <div className="pbg-mobile-label-secondary">Label Secondary</div>)
+  .add('pbg-mobile-small-text', () => <div className="pbg-mobile-small-text">Small Text</div>)
+  .add('pbg-mobile-small-secondary', () => <div className="pbg-mobile-small-secondary">Small Secondary Text</div>)
+  .add('pbg-mobile-hint-normal', () => <div className="pbg-mobile-hint-normal">Hint Normal</div>);
