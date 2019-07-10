2b16c47ffa25a1c51fe001444f956e19c94c1e9e
Roy Chen <roychen4212@gmail.com>
2019-07-10 13:44:06 +0400

    [#166333233] Fix made list of changes by request.
    Added cardType prop which allows to have different card icons in the placeholder area.
    Added allowedCardTypes which allows to have different allowed cardTypes for different customers.
    Make CardField and DivInput fluid.

--- components/consumer/desktop/card-field/card-field.js
+++ components/consumer/desktop/card-field/card-field.js
@@ -2,34 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './style.scss';
-import BaseLabel from '../../shared/base-label';
 import Cards from './cards';
 import DivInput from '../div-input';
+import CardPlaceholder from './card-placeholder';
 
 class CardField extends React.Component {
   baseClassName = 'pbg-consumer-desktop pbg-form-field pbg-text-field pbg-card-field';
 
   static propTypes = {
     label: PropTypes.string,
+    allowedCardTypes: PropTypes.arrayOf(PropTypes.string),
+    cardType: PropTypes.oneOf(['visa', 'master', 'american_express', 'discover', 'diners_club']),
   };
 
   static defaultProps = {
-    label: 'Card Number',
+    label: null,
+    cardType: null,
+    allowedCardTypes: ['visa', 'master', 'american_express', 'discover', 'diners_club'],
   };
 
   render() {
-    const { label } = this.props;
+    const { label, allowedCardTypes, cardType } = this.props;
 
     return (
       <div className={this.baseClassName}>
-        <BaseLabel className="pbg-consumer-desktop">
+        {label && (
           <div className="pbg-card-field-label-wrapper">
-            <div className="pbg-label-with-cards">{label}</div>
-            <Cards />
+            <div className="pbg-desktop-label-normal pbg-label-with-cards">{label}</div>
+            <Cards allowedCardTypes={allowedCardTypes} />
           </div>
-        </BaseLabel>
+        )}
         <div className="pbg-card-field-wrapper">
-          <div className="pgb-placeholder-icon" />
+          <CardPlaceholder cardType={cardType} />
           <DivInput />
         </div>
       </div>
--- components/consumer/desktop/card-field/cards.js
+++ components/consumer/desktop/card-field/cards.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const Cards = () => (
+const Cards = ({ allowedCardTypes }) => (
   <div className="pbg-consumer-desktop pbg-cards-wrapper">
-    <div className="pbg-visa-card pgb-card" />
-    <div className="pbg-master-card pgb-card" />
-    <div className="pbg-amex-card pgb-card" />
-    <div className="pbg-discover-card pgb-card" />
-    <div className="pbg-dinners-club-card pgb-card" />
+    {allowedCardTypes.indexOf('visa') !== -1 && <div className="pbg-visa-card pgb-card" />}
+    {allowedCardTypes.indexOf('master') !== -1 && <div className="pbg-master-card pgb-card" />}
+    {allowedCardTypes.indexOf('american_express') !== -1 && <div className="pbg-amex-card pgb-card" />}
+    {allowedCardTypes.indexOf('discover') !== -1 && <div className="pbg-discover-card pgb-card" />}
+    {allowedCardTypes.indexOf('diners_club') !== -1 && <div className="pbg-dinners-club-card pgb-card" />}
   </div>
 );
+
+Cards.propTypes = {
+  allowedCardTypes: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Cards;
