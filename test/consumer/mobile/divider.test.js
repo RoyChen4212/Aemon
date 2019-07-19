e4fac5fad536b5e2f3b2dccc96406925ec6fdaf0
Roy Chen <roychen4212@gmail.com>
2019-07-19 14:50:16 -0300

    [#167083268] Update mobile divider

--- test/consumer/mobile/divider.test.js
+++ test/consumer/mobile/divider.test.js
@@ -4,21 +4,15 @@ import { shallow } from 'enzyme';
 import Divider from '../../../components/consumer/mobile/divider';
 
 describe('Divider', () => {
-  it('should have class pbg-consumer-desktop', () => {
+  it('should have correct classes', () => {
     const wrapper = shallow(<Divider />);
     expect(wrapper.hasClass('pbg-consumer-mobile')).to.be.true;
-  });
-
-  it('should have class pbg-divider', () => {
-    const wrapper = shallow(<Divider />);
     expect(wrapper.hasClass('pbg-divider')).to.be.true;
   });
 
   it('should accept className prop', () => {
-    const className = 'my-3';
+    const className = 'custom-classname';
     const wrapper = shallow(<Divider className={className} />);
     expect(wrapper.hasClass(className)).to.be.true;
-    expect(wrapper.hasClass('pbg-consumer-mobile')).to.be.true;
-    expect(wrapper.hasClass('pbg-divider')).to.be.true;
   });
 });
