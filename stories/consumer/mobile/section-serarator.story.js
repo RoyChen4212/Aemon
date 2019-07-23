c949135662690b7919d17e2829df58fe16eca7b9
Roy Chen <roychen4212@gmail.com>
2019-07-23 22:31:54 +0400

    [#167411802] Fix added background-color to section separator component

--- stories/consumer/mobile/section-serarator.story.js
+++ stories/consumer/mobile/section-serarator.story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withContainer, wrapStory, withGreyContainer } from '../../util/decorators';
+import { withContainer, wrapStory } from '../../util/decorators';
 import SectionSeparator from '../../../components/consumer/mobile/section-separator';
 
 import '../../style.css';
@@ -9,11 +9,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 storiesOf('Consumer/Mobile/Atomic Components', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .addDecorator(withGreyContainer)
   .add('section-separator', () => (
     <div>
-      <div style={{ border: '1px solid black' }}>Section1</div>
+      <div>Section1</div>
       <SectionSeparator />
-      <div style={{ border: '1px solid black' }}>Section2</div>
+      <div>Section2</div>
     </div>
   ));
