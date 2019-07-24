795cb524af251f1b8fb9ea0c9e3f43a286326948
Roy Chen <roychen4212@gmail.com>
2019-07-24 23:04:06 +0400

    [#167082980] Fix Changed the logo image to gray one. slightly changed the position of the logo and alignment.

--- components/consumer/mobile/navbar-brand/navbar-brand.js
+++ components/consumer/mobile/navbar-brand/navbar-brand.js
@@ -7,10 +7,10 @@ const baseClassName = 'pbg-consumer-mobile pbg-navbar-brand';
 
 const NavbarBrand = ({ merchantLogoUrl, poweredByText, menuIconVisible, onMenuClick }) => (
   <div className={baseClassName}>
-    <div className="pbg-navbar-brand-icon" style={{ backgroundImage: `url(${merchantLogoUrl})` }} />
     <div className="pbg-navbar-text-container">
+      <div className="pbg-navbar-brand-icon" style={{ backgroundImage: `url(${merchantLogoUrl})` }} />
       <span className="pbg-navbar-brand-text">{poweredByText}</span>
-      <div className="pbg-navbar-brand-pbg-icon pbg-logo-powered-by" />
+      <div className="pbg-navbar-brand-pbg-icon pbg-logo-powered-by pbg-logo-powered-by-gray" />
     </div>
     {menuIconVisible && <div className="pbg-navbar-brand-menu pbg-icon-menu-small-blue" onClick={onMenuClick} />}
   </div>
