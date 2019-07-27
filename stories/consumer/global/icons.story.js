262ad151ef57b99e4f5e92ed0c5209dcbc2152b7
Roy Chen <roychen4212@gmail.com>
2019-07-28 00:47:54 +0400

    [#167479803] Added small icons global assets 5. Payment

--- stories/consumer/global/icons.story.js
+++ stories/consumer/global/icons.story.js
@@ -10,38 +10,48 @@ import 'bootstrap/dist/css/bootstrap.css';
 storiesOf('Consumer/Global/Icons/Small/Email & Messaging', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('envelope-closed-small', () => <Icons className="pbg-envelope-closed-small" />)
-  .add('speech-bubble-small', () => <Icons className="pbg-speech-bubble-small" />)
+  .add('envelope-closed-small', () => <Icons className="pbg-icon-envelope-closed-small" />)
+  .add('speech-bubble-small', () => <Icons className="pbg-icon-speech-bubble-small" />);
 
 storiesOf('Consumer/Global/Icons/Small/Editing', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('edit-small', () => <Icons className="pbg-edit-small" />)
-  .add('org-controls-small-gray', () => <Icons className="pbg-org-controls-small-gray" />)
-  .add('org-controls-small-blue', () => <Icons className="pbg-org-controls-small-blue" />)
-  .add('cog-small-gray', () => <Icons className="pbg-cog-small-gray" />)
-  .add('cog-small-blue', () => <Icons className="pbg-cog-small-blue" />)
+  .add('edit-small', () => <Icons className="pbg-icon-edit-small" />)
+  .add('org-controls-small-gray', () => <Icons className="pbg-icon-org-controls-small-gray" />)
+  .add('org-controls-small-blue', () => <Icons className="pbg-icon-org-controls-small-blue" />)
+  .add('cog-small-gray', () => <Icons className="pbg-icon-cog-small-gray" />)
+  .add('cog-small-blue', () => <Icons className="pbg-icon-cog-small-blue" />);
 
 storiesOf('Consumer/Global/Icons/Small/Group Invites', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('envelope-open-small-gray', () => <Icons className="pbg-envelope-open-small-gray" />)
-  .add('envelope-open-small-blue', () => <Icons className="pbg-envelope-open-small-blue" />)
+  .add('envelope-open-small-gray', () => <Icons className="pbg-icon-envelope-open-small-gray" />)
+  .add('envelope-open-small-blue', () => <Icons className="pbg-icon-envelope-open-small-blue" />);
 
 storiesOf('Consumer/Global/Icons/Small/Group Creation', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('rocket-small', () => <Icons className="pbg-rocket-small" />)
+  .add('rocket-small', () => <Icons className="pbg-icon-rocket-small" />);
 
 storiesOf('Consumer/Global/Icons/Small/Users', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('add-user-small-blue', () => <Icons className="pbg-add-user-small-blue" />)
-  .add('add-user-small-gray', () => <Icons className="pbg-add-user-small-gray" />)
-  .add('org-identifier-small', () => <Icons className="pbg-org-identifier-small" />)
-  .add('user-contribution-small', () => <Icons className="pbg-user-contribution-small" />)
-  .add('user-small', () => <Icons className="pbg-user-small" />)
-  .add('users-small', () => <Icons className="pbg-users-small" />)
+  .add('add-user-small-blue', () => <Icons className="pbg-icon-add-user-small-blue" />)
+  .add('add-user-small-gray', () => <Icons className="pbg-icon-add-user-small-gray" />)
+  .add('org-identifier-small', () => <Icons className="pbg-icon-org-identifier-small" />)
+  .add('user-contribution-small', () => <Icons className="pbg-icon-user-contribution-small" />)
+  .add('user-small', () => <Icons className="pbg-icon-user-small" />)
+  .add('users-small', () => <Icons className="pbg-icon-users-small" />);
 
-
-
+storiesOf('Consumer/Global/Icons/Small/Payment', module)
+  .addDecorator(wrapStory)
+  .addDecorator(withContainer)
+  .add('credit-card-small', () => <Icons className="pbg-icon-credit-card-small" />)
+  .add('lock-small', () => <Icons className="pbg-icon-lock-small" />)
+  .add('unlock-small', () => <Icons className="pbg-icon-unlock-small" />)
+  .add('price-tag-small', () => <Icons className="pbg-icon-price-tag-small" />)
+  .add('credit-card-error-small', () => <Icons className="pbg-icon-credit-card-error-small" />)
+  .add('credit-card-pending-small', () => <Icons className="pbg-icon-credit-card-pending-small" />)
+  .add('transaction-small', () => <Icons className="pbg-icon-transaction-small" />)
+  .add('credit-card-add-small', () => <Icons className="pbg-icon-credit-card-add-small" />)
+  .add('credit-card-subtract-small', () => <Icons className="pbg-icon-credit-card-subtract-small" />);
