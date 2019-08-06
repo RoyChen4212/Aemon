f8c828c36cd9b979999f210b4932898c74a8674f
Roy Chen <roychen4212@gmail.com>
2019-08-06 18:38:17 +0400

    [#167438137] Fix, removed avatar and start using online avatars.

--- stories/consumer/mobile/group-member.story.js
+++ stories/consumer/mobile/group-member.story.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withContainer, wrapStory } from '../../util/decorators';
 import GroupMember from '../../../components/consumer/mobile/group-member';
-import avatar from '../../../components/consumer/mobile/img/avatar.svg';
 
 import '../../style.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,5 +9,5 @@ import 'bootstrap/dist/css/bootstrap.css';
 storiesOf('Consumer/Mobile/Info/group-member', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
-  .add('default', () => <GroupMember fullName="Jon Doe" hint="hint" avatar={avatar} />)
-  .add('no-hint', () => <GroupMember fullName="Jon Doe" avatar={avatar} />);
+  .add('default', () => <GroupMember fullName="Jon Doe" hint="hint" avatar="https://i.pravatar.cc/32" />)
+  .add('no-hint', () => <GroupMember fullName="Jon Doe" avatar="https://i.pravatar.cc/32" />);
