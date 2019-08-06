417855b620a2dc666150df44f9cb266c2dbc14ba
Roy Chen <roychen4212@gmail.com>
2019-08-06 16:28:29 +0400

    [#167717483] Added ShareLink components long link case

--- stories/consumer/mobile/share-link.story.js
+++ stories/consumer/mobile/share-link.story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withContainer, wrapStory } from '../../util/decorators';
+import { withContainer, wrapStory, withMobileSizing } from '../../util/decorators';
 import ShareLink from '../../../components/consumer/mobile/share-link';
 
 import '../../style.css';
@@ -10,3 +10,10 @@ storiesOf('Consumer/Mobile/Onboarding/invite-contributors-link', module)
   .addDecorator(wrapStory)
   .addDecorator(withContainer)
   .add('share-link', () => <ShareLink href="http://paybygroup.com/dshjahghfd" />);
+
+storiesOf('Consumer/Mobile/Onboarding/invite-contributors-link', module)
+  .addDecorator(wrapStory)
+  .addDecorator(withContainer)
+  .addDecorator(withMobileSizing)
+  .add('share-link/long-link', () => <ShareLink href="http://paybygroup.com/dshjahghfdadsasdsaasdasadaadsadsadasdasdasdasdasdasdasda" />);
+
