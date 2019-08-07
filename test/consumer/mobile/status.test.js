e951032ce13cbd0d9fc82fc89310db9838fd0f93
Roy Chen <roychen4212@gmail.com>
2019-08-07 13:30:23 +0400

    [#167411506] Fixed test on status component for mobile

--- test/consumer/mobile/status.test.js
+++ test/consumer/mobile/status.test.js
@@ -18,14 +18,14 @@ describe('status', () => {
   it('should have correct label, value and tooltip when they provided', () => {
     const wrapper = shallow(<Status label={label} value={value} iconType={iconTypes.PRICE_TAG} tooltip={tooltip} />);
     expect(wrapper.find('.pbg-mobile-small-normal').text()).to.equal(label);
-    expect(wrapper.find('.pbg-mobile-normal').text()).to.equal(value);
+    expect(wrapper.find('.pbg-mobile-label-normal').text()).to.equal(value);
     expect(wrapper.find('.pbg-icon-question-mark-small-blue').length).to.equal(1);
   });
 
   it('should not have tooltip when its not provided', () => {
     const wrapper = shallow(<Status label={label} value={value} iconType={iconTypes.PRICE_TAG} />);
     expect(wrapper.find('.pbg-mobile-small-normal').text()).to.equal(label);
-    expect(wrapper.find('.pbg-mobile-normal').text()).to.equal(value);
+    expect(wrapper.find('.pbg-mobile-label-normal').text()).to.equal(value);
     expect(wrapper.find('.pbg-icon-question-mark-small-blue').length).to.equal(0);
   });
 });
