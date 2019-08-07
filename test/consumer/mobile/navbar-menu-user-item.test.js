074a9ff9d5b0ccad610e2c1cd7034706ff72093a
Roy Chen <roychen4212@gmail.com>
2019-08-07 13:18:20 +0400

    [#167331548] Added missing test for navbar-menu-user-item component

--- test/consumer/mobile/navbar-menu-user-item.test.js
+++ test/consumer/mobile/navbar-menu-user-item.test.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import NavbarMenuUserItem from '../../../components/consumer/mobile/navbar-menu-user-item';
+import Avatar from '../../../components/consumer/mobile/avatar';
 
 describe('navbar-menu-user-item', () => {
   it('should have correct class names', () => {
-    const wrapper = shallow(<NavbarMenuUserItem />);
+    const avatar = 'avatar';
+    const fullName = 'avatar';
+    const hint = 'hint';
+    const wrapper = shallow(<NavbarMenuUserItem avatar={avatar} fullName={fullName} hint={hint} />);
     expect(wrapper.hasClass('pbg-consumer-mobile')).to.be.true;
     expect(wrapper.hasClass('pbg-navbar-menu-user-item')).to.be.true;
+  });
+
+  it('should have correct avatar, hint and fullName', () => {
+    const avatar = 'avatar';
+    const fullName = 'avatar';
+    const hint = 'hint';
+    const wrapper = shallow(<NavbarMenuUserItem avatar={avatar} fullName={fullName} hint={hint} />);
+    expect(wrapper.find(Avatar).prop('fullName')).to.equal(fullName);
+    expect(wrapper.find(Avatar).prop('src')).to.equal(avatar);
+    expect(wrapper.find('.pbg-mobile-small-normal').text()).to.equal(hint);
+    expect(wrapper.find('.pbg-mobile-heading-2').text()).to.equal(fullName);
   });
 });
