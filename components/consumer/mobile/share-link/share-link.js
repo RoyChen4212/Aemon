aa3984917c434133985ffb30ec8f8d99040ec5b7
Roy Chen <roychen4212@gmail.com>
2019-08-13 11:17:29 +0400

    [#167411760] Made share link text autoselect

--- components/consumer/mobile/share-link/share-link.js
+++ components/consumer/mobile/share-link/share-link.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import './style.scss';
 
-const baseClassName = 'pbg-consumer-mobile pbg-share-link';
+class ShareLink extends PureComponent {
+  static baseClassName = 'pbg-consumer-mobile pbg-share-link';
 
-const ShareLink = ({ label }) => (
-  <div className={baseClassName}>
-    <span className="pbg-icon-link-small-gray" />
-    <span className="pbg-mobile-label-normal">{label}</span>
-  </div>
-);
+  onClick = e => {
+    let range;
+    let selection;
+
+    if (window.getSelection && document.createRange) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(e.target);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    } else if (document.selection && document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(e.target);
+      range.select();
+    }
+    e.target.select();
+  };
+
+  render() {
+    const { label } = this.props;
+    return (
+      <div className={ShareLink.baseClassName}>
+        <span className="pbg-icon-link-small-gray" />
+        <span className="pbg-mobile-label-normal" onClick={this.onClick}>
+          {label}
+        </span>
+      </div>
+    );
+  }
+}
 
 ShareLink.propTypes = {
   label: PropTypes.string.isRequired,
